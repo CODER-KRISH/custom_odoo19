e508f758f5774c1488f0db64de710d892f75d9aa
Krish Prajapati <kaprajapati2495@gmail.com>
2026-05-07 18:57:23 +0530

    All Done!

--- git_demo_folder/git_demo/main.py
+++ git_demo_folder/git_demo/main.py
@@ -3,7 +3,10 @@ print("Hello World!")
 print("This is the first changes!")
 
 def method():
-    print("This is the new method hello!")
+    print("This is the method hello!")
 
 def action_view_all_properties():
     print("These are all properties!")
+
+def new_method():
+    print("This is a new method!")

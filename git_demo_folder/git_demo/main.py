95b9f97bc6ba14ce7c8219727de5690e0531270f
Krish Prajapati <kaprajapati2495@gmail.com>
2026-05-08 11:04:31 +0530

    Modified

--- git_demo_folder/git_demo/main.py
+++ git_demo_folder/git_demo/main.py
@@ -4,7 +4,6 @@ print("This is the first changes!")
 
 def method():
     print("This is the method hello!")
-    print("Test Product!")
 
 def action_view_all_properties():
     print("These are all properties!")

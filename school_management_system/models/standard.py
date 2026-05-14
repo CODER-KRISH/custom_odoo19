21227ad05208c7e0f23ef2185752bc3062cdfb8d
Krish Prajapati <kaprajapati2495@gmail.com>
2026-05-14 15:43:56 +0530

    Modified All Files

--- school_management_system/models/standard.py
+++ school_management_system/models/standard.py
@@ -18,3 +18,60 @@ class Standard(models.Model):
     # Connects to Subjects
     # relation needed to add multiple subjects in one standard
     subject_ids = fields.One2many('subject', 'standard_id', string='Subjects')
+
+
+    """Smart Buttons for Students"""
+    standard_student_count = fields.Integer(compute='_compute_students_count')
+
+    def _compute_students_count(self):
+        self.standard_student_count = self.env['student'].search_count([
+            ('standard_id.class_name', '=', self.class_name)
+        ])
+
+    def action_view_student(self):
+        domain = [('standard_id.class_name', '=', self.class_name)]
+
+        action = {
+            'type': 'ir.actions.act_window',
+            'name': f'Class {self.class_name} Students',
+            'res_model': 'student',
+            'view_mode': "list",
+            'views': [(False, "list"), (False, "form")],
+            'domain': domain,
+        }
+
+        if len(self.student_ids) == 1:
+            action.update({
+                "views": [(False, "form")],
+                "res_id": self.student_ids.id
+            })
+
+        return action
+
+    """Smart Buttons for Subjects"""
+    standard_subjects_count = fields.Integer(compute='_compute_subjects_count')
+
+    def _compute_subjects_count(self):
+        self.standard_subjects_count = self.env['subject'].search_count([
+            ('standard_id.class_name', '=', self.class_name)
+        ])
+
+    def action_view_subject(self):
+        domain = [('standard_id.class_name', '=', self.class_name)]
+
+        action = {
+            'type': 'ir.actions.act_window',
+            'name': f'Class {self.class_name} Subjects',
+            'res_model': 'subject',
+            'view_mode': "list",
+            'views': [(False, "list"), (False, "form")],
+            'domain': domain,
+        }
+
+        if len(self.subject_ids) == 1:
+            action.update({
+                "views": [(False, "form")],
+                "res_id": self.subject_ids.id
+            })
+
+        return action

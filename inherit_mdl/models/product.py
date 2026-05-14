3717a2705af8ac44709d653fd7a8d93e008b5411
Krish Prajapati <kaprajapati2495@gmail.com>
2026-05-14 11:46:33 +0530

    Modified All Files

--- inherit_mdl/models/product.py
+++ inherit_mdl/models/product.py
@@ -4,4 +4,4 @@ from odoo.exceptions import AccessError, ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    special = fields.Boolean(string="Special")
+    special = fields.Boolean(string="Special", help="inherited - To show the product is special")

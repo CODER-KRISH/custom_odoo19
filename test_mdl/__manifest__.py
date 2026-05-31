a9f30640603654734caa858a8501a75f240428a8
Krish Prajapati <kaprajapati2495@gmail.com>
2026-05-31 09:14:54 +0530

    Updation Complete

--- test_mdl/__manifest__.py
+++ test_mdl/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    'name': "Product Lifecycle",
-    'summary': "Product Lifecycle",
+    'name': "Test Module",
+    'summary': "Test Module",
     'author': 'Krish Prajapati',
     'category': 'Product',
     'version': '19.0.1.0.0',

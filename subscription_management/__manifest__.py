ee9472fda1b26cc1a407b6a472fb35471604ffb1
Krish Prajapati <kaprajapati2495@gmail.com>
2026-06-10 16:49:33 +0530

    Added

--- subscription_management/__manifest__.py
+++ subscription_management/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    'name': "Subscription Management",
-    'summary': "Summery of Subscription Management System",
-    'author': 'Krish Prajapati',
-    'category': 'Subscription',
-    'version': '19.0.1.0.0',
-    'depends': ['base', 'mail', 'account'],
-    'data': [
+    "name": "Subscription Management",
+    "summary": "Summery of Subscription Management System",
+    "author": 'Krish Prajapati',
+    "category": 'subscription',
+    "version": '19.0.1.0.0',
+    "depends": ['base', 'mail', 'account'],
+    "data": [
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/subscription_order_view.xml',
@@ -16,7 +16,7 @@
         'report/subscription_order_report.xml',
         'views/menu.xml',
     ],
-    "application": True,
     "installable": True,
-    "sequence": -1999
+    "application": False,
+    "auto_install": False,
 }
